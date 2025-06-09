ebbcb582b1a283752a5b4f4dc15ecf4186b7f871
Shi0-X <jshi0x@gmail.com>
2025-06-09 10:51:12 -0500

    linter fix

--- server/migrations/20210711232742_create_labels_table.js
+++ server/migrations/20210711232742_create_labels_table.js
@@ -3,12 +3,12 @@
  * @param {import('knex')} knex
  */
 export const up = (knex) => (
-  knex.schema.createTable('labels', (table) => {
-    table.increments('id').primary();
-    table.string('name').notNullable();
-    table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at').defaultTo(knex.fn.now());
-  })
-);
-
-export const down = (knex) => knex.schema.dropTable('labels');
+    knex.schema.createTable('labels', (table) => {
+      table.increments('id').primary();
+      table.string('name').notNullable();
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+  );
+  
+  export const down = (knex) => knex.schema.dropTable('labels');
--- server/migrations/20210711232754_create_tasks_labels.js
+++ server/migrations/20210711232754_create_tasks_labels.js
@@ -3,14 +3,16 @@
  * @param {import('knex')} knex
  */
 export const up = (knex) => (
-  knex.schema.createTable('tasks_labels', (table) => {
-    table.increments('id').primary();
-    table.integer('task_id').unsigned().notNullable()
-      .references('id')
-      .inTable('tasks')
-      .onDelete('CASCADE');
-    table.integer('label_id').unsigned().notNullable()
-      .references('id')
-      .inTable('labels')
-      .onDelete('RESTRICT');
-    table.unique(['task_id', 'label_id']);
+    knex.schema.createTable('tasks_labels', (table) => {
+      table.increments('id').primary();
+      table.integer('task_id').unsigned().notNullable()
+        .references('id').inTable('tasks').onDelete('CASCADE');
+      table.integer('label_id').unsigned().notNullable()
+        .references('id').inTable('labels').onDelete('RESTRICT');
+      table.unique(['task_id', 'label_id']);
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+  );
+  
+  export const down = (knex) => knex.schema.dropTable('tasks_labels');
