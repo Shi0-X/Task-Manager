4c6cab4612df4b45ef1a3842e599988d1f9c494e
Shi0-X <jshi0x@gmail.com>
2025-06-09 10:44:15 -0500

    Update 20200716171734_create_users_table.js

--- server/migrations/20200716171734_create_users_table.js
+++ server/migrations/20200716171734_create_users_table.js
@@ -1,14 +1,15 @@
 // server/migrations/20210711224051_create_users_table.js
 // @ts-check
 
-export const up = (knex) => knex.schema.createTable('users', (table) => {
-  table.increments('id').primary();
-  table.string('first_name').notNullable(); // <— lo agregamos
-  table.string('last_name').notNullable(); // <— lo agregamos
-  table.string('email').notNullable();
-  table.string('password_digest').notNullable();
-  table.timestamp('created_at').defaultTo(knex.fn.now());
-  table.timestamp('updated_at').defaultTo(knex.fn.now());
-});
+export const up = (knex) =>
+  knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('first_name').notNullable();   // <— lo agregamos
+    table.string('last_name').notNullable();    // <— lo agregamos
+    table.string('email').notNullable();
+    table.string('password_digest').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
 
 export const down = (knex) => knex.schema.dropTable('users');
